dc04002ce6026ec2f0b927ce3da6fd0c8411800b
hannahrhugh <30412121+hannahrhugh@users.noreply.github.com>
2017-11-06 09:03:27 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('#hamburger').on('click', function() {
+		$('nav').slideToggle();
+	});
+
+	$(window).on('resize', function() {
+		if ($(window).width() >= 1000 && !$('nav').is(':visible')) {
+			$('nav').show();
+		} else if ($(window).width() < 1000 && $('nav').is(':visible')) {
+			$('nav').hide();
+		}
+	});
+});
